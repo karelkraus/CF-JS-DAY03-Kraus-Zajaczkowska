988eb84819cab413cf6d96b54bf936d2ae2c1b36
eva anna <eva.zajaczkowska@gmail.com>
2020-09-22 12:54:24 +0200

    basic2 and basic3

---
+++ script2.js
@@ -0,0 +1,9 @@
+function ageCalculator(BirthYear) {
+	const x = 2020;
+	var calculation = x - BirthYear;
+
+	document.getElementById("basic2").innerHTML = calculation + "<br>";
+	return
+}
+	ageCalculator(1990);
+	
